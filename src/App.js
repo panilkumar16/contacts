661e618e78bb045f56f8ed0f0cfa0345f2357ac9
Anil Kumar <panilkumar16@gmail.com>
2017-11-03 23:58:51 -0700

    feat: Add Router Component to route between urls
    Use react-router-dom for BrowserRoute, Link, and Route so
    the urls are tracked and rendered based on path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
@@ -22,10 +24,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts 
-          onDeleteContact={this.removeContact} 
-          contacts={this.state.contacts}
-        />
+        <Route exact path="/" render={() => (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )}/>
+        <Route path="/create" component={CreateContact}/>
       </div>
     )
   }
